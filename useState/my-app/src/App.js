b2e3a5f28c7e35b48952e6c88a3edc6a920b89b1
dudhatmitesh <miteshdudhat5@gmail.com>
2024-07-10 16:16:57 +0530

    second

---
+++ useState/my-app/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>
+          abuna1985/simole-react- <br></br> countr
+        </h1>
+       <p>A simple countr written in React.js</p>
+        <img src={"https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg"} className="App-logo" alt="logo" />
+        
+          {/* <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a> */}
+          <i class="fa-sharp fa-thin fa-user-group-crown"></i> 
+      </header>
+    </div>
+  );
+}
+
+export default App;
+{/* <img src={"https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg"} className="app-img" alt="img"></img> */}
